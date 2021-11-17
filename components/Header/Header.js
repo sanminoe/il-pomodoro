58aba731fbccc5e37ecc0741cdc75f34ea7c74b0
sanminoe <megasantiago132@gmail.com>
2021-11-17 22:50:55 +0100

    added tooltip

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { useContext } from 'react';
 
 import ConfigContext from '../../store/configStore';
-
+import style from './style.module.css';
 export default (props) => {
 	const configCtx = useContext(ConfigContext);
 	const router = useRouter();
@@ -14,32 +14,34 @@ export default (props) => {
 		>
 			<div className="flex justify-between w-full mt-2">
 				<div>
-					<h1>Pomo</h1>
+					<h1 className="after:block after:bg-red-600 after:contet-['Title']">Pomo</h1>
 				</div>
 				<div>
 					{router.route === '/' && (
-						<button>
+						<button className={style.settingButton}>
 							<Link href="/setting">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									className="h-6 w-6"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke="currentColor"
-								>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-									/>
-									<path
-										strokeLinecap="round"
-										strokeLinejoin="round"
-										strokeWidth={2}
-										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-									/>
-								</svg>
+								<div className="flex">
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										className="h-6 w-6 transform hover:rotate-90 transition-transform"
+										fill="none"
+										viewBox="0 0 24 24"
+										stroke="currentColor"
+									>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth={2}
+											d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+										/>
+										<path
+											strokeLinecap="round"
+											strokeLinejoin="round"
+											strokeWidth={2}
+											d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+										/>
+									</svg>
+								</div>
 							</Link>
 						</button>
 					)}
